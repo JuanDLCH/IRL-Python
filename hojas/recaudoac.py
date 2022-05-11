4eabe9496377efefc128681452466b89feb9f2db
JuanDLCH <juandiego0329@gmail.com>
2022-05-11 15:21:46 -0500

    RecaudoAC completo sin testear segunda vez

--- hojas/recaudoac.py
+++ hojas/recaudoac.py
@@ -1,6 +1,7 @@
 from utils.globals import *
 import xlwings as xw
 import pandas as pd
+from utils.fecha import Fecha
 
 doc = 'INFORME INDIVIDUAL DE LAS CAPTACIONES (MODIFICADO)'
 hoja = 'Recaudo de Ahorro Contractual'
@@ -8,14 +9,18 @@ cuentas = [ 212505,212510,212515, 212520 ]
 
 
 def recaudoac(fecha: Fecha, primeraVez: bool , wb: xw.Book):
-    print ('Diligenciando Recaudo de Ahorro C ontractual')
+    print ('Diligenciando Recaudo de Ahorro Contractual')
+    archivos = os.listdir('{}/Archivos/{}'.format(rutaRobot, doc))
     ws = wb.sheets[hoja]
+    mes = '0' + str(fecha.mes) if fecha.mes < 10 else str(fecha.mes)
+    ws.range('B6').value = '{}/{}/{}'.format(fecha.add_months(1).add_days(-1).dia, mes, fecha.anio)
     if primeraVez:
         fechaAux = fecha.add_months(-24)
+
         for i in range(25):
-            dia = '0' + str(fechaAux.dia) if fechaAux.dia < 10 else str(fechaAux.dia)
-            ws.range('A' + str(i + 12)).value = '{}/{}/{}'.format(dia, fechaAux.mes, fechaAux.anio)
-            archivos = os.listdir('{}/Archivos/{}'.format(rutaRobot, doc))
+            dia = fechaAux.add_months(1).add_days(-1).dia
+            mes = '0' + str(fechaAux.mes) if fechaAux.mes < 10 else str(fechaAux.mes)
+            ws.range('A' + str(i + 12)).value = '{}/{}/{}'.format(dia, mes, fechaAux.anio)
             archivo = [archivo for archivo in archivos if fechaAux.as_Text() in archivo][0]
             archivo = os.path.join('{}/Archivos/{}'.format(rutaRobot, doc), archivo)
 
@@ -27,4 +32,24 @@ def recaudoac(fecha: Fecha, primeraVez: bool , wb: xw.Book):
 
             fechaAux = fechaAux.add_months(1)
     else:
-        print('xd')
+        mes = '0' + str(fecha.mes) if fecha.mes < 10 else str(fecha.mes)
+        dia = fecha.add_months(1).add_days(-1).dia
+        ultimaFila = ws.range('A' + str(ws.api.UsedRange.Rows.Count)).end('up').row
+        ws.range('A' + str(ultimaFila + 1)).value = '{}/{}/{}'.format(fecha.dia, mes, fecha.anio)
+        archivo = [archivo for archivo in archivos if fecha.as_Text() in archivo][0]
+        archivo = os.path.join('{}/Archivos/{}'.format(rutaRobot, doc), archivo)
+
+        tabla = pd.read_csv(archivo, usecols=['CodigoContable', 'Saldo'], encoding='ANSI', sep=';', skiprows=3)
+        tabla = tabla.loc[tabla['CodigoContable'].isin(cuentas)]
+        suma = tabla['Saldo'].sum()
+
+        ws.range('B' + str(ultimaFila + 1)).value = suma
+
+        # Arrastrar formulas de la fila anterior
+        ws.range("A{row}:I{row}".format(row = ultimaFila)).api.AutoFill(ws.range("A{row}:F{row}".format(row=ultimaFila + 1)).api, 0 )
+        ws.range('A12:I' + str(ultimaFila + 1)).api.Borders(11).LineStyle = 1 #Linea Vertical
+        ws.range('A12:I' + str(ultimaFila + 2)).api.Borders(12).LineStyle = 1 #Linea Horizontal
+        # Cambiar fuente
+        ws.range('A12:I' + str(ultimaFila + 1)).api.Font.Name = 'Verdana'
+
+
